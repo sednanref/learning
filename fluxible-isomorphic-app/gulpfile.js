b595d52c09184e5569a016ccf4bd3abcc9c04a49
Luis Fernandes <sednanref2004@gmail.com>
2016-11-03 13:21:59 +0100

    Configuring the gulp file

--- fluxible-isomorphic-app/gulpfile.js
+++ fluxible-isomorphic-app/gulpfile.js
@@ -1,5 +1,19 @@
 var gulp = requre('gulp');
+var broserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+
+var config = {
+  entry_file: './source/client.js',
+  destination_folder: './build/',
+  destination_file: 'bundle.js'
+};
 
 gulp.task('build', function() {
-
+  browserify(config.entry_file)
+    .transform(reactify)
+    .bundle()
+    .on('error', console.error.bind(console))
+    .pipe(source(config.destination_file))
+    .pipe(gulp.dest(config.destination_folder));
 });
